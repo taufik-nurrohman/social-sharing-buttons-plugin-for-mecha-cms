06eff66bc58a5264e0ecba00a4523a299aea3faa
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-07-21 19:29:06 +0700

    Internal `scraps` Folder
    Put the cache files in the internal `scraps` folder instead.

--- social-sharing-buttons/launch.php
+++ social-sharing-buttons/launch.php
@@ -28,7 +28,7 @@ Widget::add('shareButtons', function() use($config, $ssb_config, $ssb_button) {
     }
     Session::set('cookie:share_button_cargo', array($url, $title, $description, $image));
     $html = '<span class="share-button-group">';
-    $cache_path = CACHE . DS . 'plugin-' . File::B(__DIR__) . DS . md5($url) . '.cache';
+    $cache_path = PLUGIN . DS . File::B(__DIR__) . DS . 'scraps' . DS . md5($url) . '.cache';
     $counters = File::open($cache_path)->unserialize(array());
     foreach($ssb_button as $key => $value) {
         // no cache
@@ -43,7 +43,7 @@ Widget::add('shareButtons', function() use($config, $ssb_config, $ssb_button) {
         $html .= '<a class="share-button share-button-' . $key . '" href="' . $config->url . '/share/to:' . $key . '"' . (Asset::loaded($config->protocol . ICON_LIBRARY_PATH) && isset($value['icon']) ? ' data-icon="' . $value['icon'] . '"' : "") . '><span>' . $value['title'] . '</span>' . $count . '</a> ';
     }
     // create cache
-    if( ! File::exist($cache_path)) {
+    if( ! File::exist($cache_path) && $config->page_type !== 'manager') {
         File::serialize($counters)->saveTo($cache_path, 0600);
     }
     return trim($html) . '</span>';
@@ -80,6 +80,6 @@ Route::accept('share/to:(:any)', function($kind = "") use($config, $ssb_button) 
     }
     $param = Session::get('cookie:share_button_cargo', array($config->url, $config->title, $config->slogan, $config->url . '/favicon.ico'));
     // delete cache
-    File::open(CACHE . DS . 'plugin-' . File::B(__DIR__) . DS . md5($param[0]) . '.cache')->delete();
+    File::open(PLUGIN . DS . File::B(__DIR__) . DS . 'scraps' . DS . md5($param[0]) . '.cache')->delete();
     Guardian::kick(sprintf($ssb_button[$kind]['url'], urlencode($param[0]), urlencode(strip_tags($param[1])), urlencode(strip_tags($param[2])), urlencode($param[3])));
 });
